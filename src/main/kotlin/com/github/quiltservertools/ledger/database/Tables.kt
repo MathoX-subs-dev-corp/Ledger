54fb6aff2b6e54f362b3a6fd800db02135a0bd4b
Kamadek <kamadek@mathox1subs.fr>
2023-12-17 18:20:56 +0100

    :fire: Unused import

--- src/main/kotlin/com/github/quiltservertools/ledger/database/Tables.kt
+++ src/main/kotlin/com/github/quiltservertools/ledger/database/Tables.kt
@@ -5,7 +5,6 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.alias
 import org.jetbrains.exposed.sql.javatime.timestamp
 import java.time.Instant
